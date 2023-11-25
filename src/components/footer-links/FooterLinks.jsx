0ddc5edb5cd9224ed1d770d26e1401da00713205
mehdi-sz <mehdi.zayani15@gmail.com>
2023-11-25 11:56:21 +0100

    ajout des dossier de composants et conteneur

---
+++ src/components/footer-links/FooterLinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterLinks = () => {
+  return (
+    <div>
+        <h3>Footer Links</h3>
+    </div>
+  )
+}
+
+export default FooterLinks
---
+++ src/container/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./about.scss"
+const About = () => {
+  return (
+    <div className="about">
+        <h1>About Container</h1>
+    </div>
+  )
+}
+
+export default About
